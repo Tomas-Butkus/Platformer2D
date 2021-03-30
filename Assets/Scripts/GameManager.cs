ffdea1957ccdad8243e6dd3ec757364e56b4a1df
Tomas-Butkus <tmsbtks9@gmail.com>
2021-03-30 21:44:57 +0300

    Changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,8 +7,18 @@ public class GameManager : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
-            Debug.Log("Application Quit");
-            Application.Quit();
+            ExitGame();
         }
+    }
+
+    // Main Menu functions
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
--- README.md
+++
@@ -1,6 +0,0 @@
-# Platformer2D
-Engine: Unity  
-Programming Language: C#  
-
-Run, glide, slide, jump between walls and interact with the environment to 
-get from point A to point B.
